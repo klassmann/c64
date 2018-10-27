c3890ca27e801ed1e9c4b17e39b0200ffedd276b
Lucas Klassmann <lucasklassmann@gmail.com>
2018-10-27 11:35:48 -0300

    Adding some input and output

--- main.lua
+++ main.lua
@@ -1,29 +1,46 @@
 
+require("graphics")
 
-palette = {
-    {0, 0, 0},        -- #000000
-    {255, 255, 255},  -- #ffffff
-    {98, 98, 98},     -- #626262
-    {137, 137, 137},  -- #898989
-    {173, 173, 173},  -- #adadad
-    {159, 78, 68},    -- #9f4e44
-    {203, 126, 117},  -- #cb7e75
-    {109, 84, 18},    -- #6d5412
-    {161, 104, 60},   -- #a1683c
-    {201, 212, 135},  -- #c9d487
-    {154, 226, 155},  -- #9ae29b
-    {92, 171, 94},    -- #5cab5e
-    {106, 191, 198},  -- #6abfc6
-    {136, 126, 203},  -- #887ecb
-    {80, 69, 155},    -- #50459b
-    {160, 87, 163},   -- #a057a3
-}
 
-function get_color(index)
-    local color = palette[index]
-    return {color[1] / 255, color[2] / 255, color[3] / 255}
+output = {}
+memory = {}
+input = {}
+current_line = 1
+
+COLUMNS = 40
+
+function incr_line()
+    current_line = current_line + 1
 end
 
+function write(s)
+    local count = 0
+    local line = {}
+    for c in s:gmatch(".") do
+        table.insert(line, c)
+        count = count + 1
+        if count >= COLUMNS then
+            table.insert(output, line)
+            line = {}
+            incr_line()
+        end
+    end
+    table.insert(output, line)
+    incr_line()
+end
+
+function readline(s)
+    table.insert(input, s)
+end
+
+function backspace()
+    table.remove(input, table.getn(input))
+end
+
+function flush_readline()
+    write(table.concat(input))
+    input = {}
+end
 
 function love.load()
     font = love.graphics.newFont("C64_Pro_Mono-STYLE.ttf", 16)
@@ -31,19 +48,42 @@ function love.load()
     love.graphics.setBackgroundColor(get_color(14))
     screenHeight = love.graphics.getHeight()
     screenWidth = love.graphics.getWidth()
+
+    write("**** COMMODORE 64 BASIC V2 ****")
+    write("64K RAM SYSTEM   38911 BASIC BYTES FREE")
+    write("READY.")
+    write("")
 end
 
 function love.draw()
-    
     love.graphics.setColor(get_color(15))
     love.graphics.rectangle('fill', 30, 30, screenWidth - 60, screenHeight - 60)
-
     love.graphics.setColor(get_color(14))
-    love.graphics.print("**** COMMODORE 64 BASIC V2 ****", 110, 60)
-    love.graphics.print("64K RAM SYSTEM   38911 BASIC BYTES FREE", 80, 90)
-    love.graphics.print("READY.", 30, 120)
+    for k, v in ipairs(output) do
+        love.graphics.print(v, 30, 16 * k + 30)
+    end
+
+    local s = table.concat(input) .. "�"
+    love.graphics.print(s, 30, current_line * 16 + 30)
 end
 
 function love.keypressed(key, scancode, isrepeat)
 
+    if key:len() == 1 then
+        if key:gmatch('^[0-9a-zA-Z]{1}$') then
+            readline(key)
+        end
+    end
+
+    if key == 'space' then
+        readline(" ")
+    end
+
+    if key == 'backspace' then
+        backspace()
+    end
+
+    if key == 'return' then
+        flush_readline()
+    end
 end
